d5f03b96189f7cefcbc94626c4b51f97b3ed001b
Lucas S. Silva <57202598+LNaoEdeLixo@users.noreply.github.com>
2020-12-13 19:10:00 -0300

    somas

--- soma-funcao/soma.go
+++ soma-funcao/soma.go
@@ -1,9 +1,34 @@
 package main
 
+//Soma está funcionando!
 func Soma(numeros []int) int {
 	soma := 0
 	for _, numero := range numeros {
 		soma += numero
 	}
 	return soma
+}
+
+//SomaTudo está funcionando!
+func SomaTudo(numerosParaSomar ...[]int) []int {
+	var somas []int
+	for _, numeros := range numerosParaSomar {
+		somas = append(somas, Soma(numeros))
+	}
+
+	return somas
+}
+
+//SomaTodoOResto está funcionando!
+func SomaTodoOResto(numerosParaSomar ...[]int) []int {
+	var somas []int
+	for _, numeros := range numerosParaSomar {
+		if len(numeros) == 0 {
+			somas = append(somas, 0)
+		} else {
+			final := numeros[1:]
+			somas = append(somas, Soma(final))
+		}
+	}
+	return somas
 }
