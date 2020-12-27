77518e24886126392b95b8babdcdf03a4253a433
Lucas S. Silva <57202598+LNaoEdeLixo@users.noreply.github.com>
2020-12-26 22:01:06 -0300

    just comments

--- geometria/geometria.go
+++ geometria/geometria.go
@@ -1,7 +1,8 @@
-package main
+package geometria
 
 import "math"
 
+//Perimetro está funcionando!
 func Perimetro(retangulo Retangulo) float64 {
 	return 2 * (retangulo.Largura + retangulo.Altura)
 }
@@ -10,28 +11,34 @@ func Perimetro(retangulo Retangulo) float64 {
 // 	return retangulo.Largura * retangulo.Altura
 // }
 
+//Retangulo está funcionando
 type Retangulo struct {
 	Largura float64
 	Altura  float64
 }
 
+//Area é um metodo sendo adicionado na estrutura retangulo; quando eu chamo esse método eu passo um valor e ele é recebido aqui como "r".
 func (r Retangulo) Area() float64 {
 	return r.Largura * r.Altura
 }
 
+//Circulo é igual
 type Circulo struct {
 	Raio float64
 }
 
+//Area sendo adicionado ao circulo também
 func (c Circulo) Area() float64 {
 	return math.Pi * c.Raio * c.Raio
 }
 
+//Triangulo está funcionando!
 type Triangulo struct {
 	Base   float64
 	Altura float64
 }
 
+//Area sendo adicionado ao triangulo também
 func (t Triangulo) Area() float64 {
 	return (t.Base * t.Altura) * 0.5
 }
