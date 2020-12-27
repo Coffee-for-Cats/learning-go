10de947f6c305ea6b3f0cf14eb61096a788fdb84
Lucas S. Silva <57202598+LNaoEdeLixo@users.noreply.github.com>
2020-12-26 23:32:30 -0300

    Essa foi mais longa ou é impressão minha?

---
+++ carteira/carteira.go
@@ -0,0 +1,41 @@
+package carteira
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
+//It just work! Probably is "just" for printing better a result
+type Stringer interface {
+	String() string
+}
+
+type Carteira struct {
+	saldo Bitcoin
+}
+
+func (c *Carteira) Depositar(quantidade Bitcoin) {
+	c.saldo += quantidade
+}
+
+var ErroSaldoInsuficiente = errors.New("não é possível retirar: saldo insuficiente")
+
+func (c *Carteira) Retirar(quantidade Bitcoin) error {
+
+	if quantidade > c.saldo {
+		return ErroSaldoInsuficiente
+	}
+
+	c.saldo -= quantidade
+	return nil
+}
+
+func (c *Carteira) Saldo() Bitcoin {
+	return c.saldo
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
